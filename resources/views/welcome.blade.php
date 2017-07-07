1c664c3f5289e307de6637a0460ea5f49b588158
Minimonster_Goggag <minimonster595@gmail.com>
2017-07-07 13:38:09 +0700

    day 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    J Die Krub
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Information</a>
+                    <a href="https://laracasts.com">Activity</a>
+                    <a href="https://laravel-news.com">Times</a>
+                    <a href="https://forge.laravel.com">Contact</a>
+                    <a href="https://github.com/laravel/laravel">Mapping</a>
                 </div>
             </div>
         </div>
